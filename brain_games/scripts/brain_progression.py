5e7accbb9c3f0657421455dc53113c2764c12dcb
Никита Войтов <voitov@MacBook-Air-Nikita.local>
2024-12-11 00:10:12 +0300

    finish

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -2,8 +2,6 @@
 import random
 import prompt
 
-from .cli import welcome_user
-
 
 def brain_progression():
     print("Welcome to the Brain Games!")
@@ -12,12 +10,22 @@ def brain_progression():
     print('What number is missing in the progression?')
     i = 1
     while i <= 3:
-        result = ''
-        for i in range(random.randint(1, 50), random.randint(50, 100), random.randint(2, 8)):
+        result = []
+        for i in range(random.randint(1, 20), random.randint(50, 80), random.randint(2, 8)):
+            result.append(i)
+        answer = random.randint(0, len(result) - 1)
+        result[answer] = ".."
+        result_two = ''
+        for i in result:
             if result:
-                result += " "
-            result += i
-        print(result)
+                result_two += " "
+            result_two += str(i)
+        print(f"Question: {result_two}")
+        n = input("Your answer: ")
+        if str(answer) == str(n):
+            print("Correct")
+        else:
+            print("No Correct")
         if i == 3:
             print(f"Congratulations, {name}!")
         i += 1
@@ -25,7 +33,3 @@ def brain_progression():
 
 if __name__ == "__brain_progression__":
     brain_progression()
-
-# if __name__ == "brain_even":
-#     brain_even()
-
